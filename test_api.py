00ed830b512b3a0725a8f6a47bfbbc1a9ffc5443
Акимцев Алексей <125552026+ReVioletMist@users.noreply.github.com>
2023-12-22 03:23:38 +0500

    test_api.py

--- test_api.py
+++ test_api.py
@@ -4,21 +4,8 @@ from main import app
 
 client = TestClient(app)
 
-def test_translate_english_to_russian():
-    # Отправляем текст на английском для перевода
-    response = client.post("/translate", json={"text": "Hello, world!"})
-    
-    # Проверяем успешный код состояния
+def test_health_check():
+    response = client.get("/health")
     assert response.status_code == 200
-    
-    # Получаем переведенный текст из ответа
-    translated_text = response.json()["translation"]
-    
-    # Проверяем, что результат не пустой и содержит русские буквы
-    assert translated_text
-    assert not contains_english_letters(translated_text)
-
-def contains_english_letters(text):
-    # Проверяем, содержит ли текст хотя бы одну английскую букву
-    return any('a' <= char.lower() <= 'z' for char in text)
+    assert response.json() == {"status": "ok"}
 
