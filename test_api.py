7a1bd3ae29a70980166d7dd4d21840fad460d927
Акимцев Алексей <125552026+ReVioletMist@users.noreply.github.com>
2023-12-22 01:56:06 +0500

    test_api.py

---
+++ test_api.py
@@ -0,0 +1,23 @@
+import json
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_translate():
+    # Тестирование успешного запроса
+    data = {"text": "Hello"}
+    response = client.post("/translate", json=data)
+    assert response.status_code == 200
+    assert "translation" in response.json()
+
+def test_translate_error():
+    # Тестирование ошибки сервера
+    data = {"text": ""}
+    response = client.post("/translate", json=data)
+    assert response.status_code == 500
+
+    # Тестирование ошибки запроса
+    data = {"wrong_key": "Hello"}
+    response = client.post("/translate", json=data)
+    assert response.status_code == 422
