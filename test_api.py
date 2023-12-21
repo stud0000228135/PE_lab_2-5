7c085b8994726829d6d1a269d1a78a2a44f87c3f
Акимцев Алексей <125552026+ReVioletMist@users.noreply.github.com>
2023-12-22 03:20:23 +0500

    test_api.py

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from FastAPI import app
+from main import app
 
 client = TestClient(app)
 
@@ -16,13 +16,9 @@ def test_translate_english_to_russian():
     
     # Проверяем, что результат не пустой и содержит русские буквы
     assert translated_text
-    assert contains_russian_letters(translated_text)
+    assert not contains_english_letters(translated_text)
 
-def contains_russian_letters(text):
-    # Является ли символ кириллическим
-    def is_cyrillic(char):
-        return 'а' <= char.lower() <= 'я' or char.lower() == 'ё'
-    
-    # Проверяем, содержит ли текст хотя бы одну русскую букву
-    return any(is_cyrillic(char) for char in text)
+def contains_english_letters(text):
+    # Проверяем, содержит ли текст хотя бы одну английскую букву
+    return any('a' <= char.lower() <= 'z' for char in text)
 
