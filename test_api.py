84dba92f483b8c936803cdcc5b6094b720102a7a
Акимцев Алексей <125552026+ReVioletMist@users.noreply.github.com>
2023-12-22 02:54:09 +0500

    test_api.py

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
-import json
+import pytest
 from fastapi.testclient import TestClient
-from FastAPI import app 
+from main import app
 
 client = TestClient(app)
 
@@ -12,3 +12,15 @@ def test_translate_success():
 def test_translate_error():
     response = client.post("/translate", json={"text": "Invalid text that causes an error"})
     assert response.status_code == 500
+    assert "Internal Server Error" in response.text
+
+def test_translate_russian_to_english():
+    response = client.post("/translate", json={"text": "Привет, мир!"})
+    assert response.status_code == 200
+    assert "translation" in response.json()
+    translated_text = response.json()["translation"]
+    assert is_english_text(translated_text)
+
+# Вспомогательная функция для проверки, что текст является английским
+def is_english_text(text):
+    return not any(ord(char) > 127 for char in text)
