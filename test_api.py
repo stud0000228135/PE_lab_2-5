e2a181f747547082273fa42dd67c00e458f22e82
Акимцев Алексей <125552026+ReVioletMist@users.noreply.github.com>
2023-12-22 02:49:03 +0500

    test_api.py

--- test_api.py
+++ test_api.py
@@ -4,20 +4,11 @@ from FastAPI import app
 
 client = TestClient(app)
 
-def test_translate():
-    # Тестирование успешного запроса
-    data = {"text": "Hello"}
-    response = client.post("/translate", json=data)
+def test_translate_success():
+    response = client.post("/translate", json={"text": "Hello, world!"})
     assert response.status_code == 200
     assert "translation" in response.json()
 
 def test_translate_error():
-    # Тестирование ошибки сервера
-    data = {"text": ""}
-    response = client.post("/translate", json=data)
+    response = client.post("/translate", json={"text": "Invalid text that causes an error"})
     assert response.status_code == 500
-
-    # Тестирование ошибки запроса
-    data = {"wrong_key": "Hello"}
-    response = client.post("/translate", json=data)
-    assert response.status_code == 422
